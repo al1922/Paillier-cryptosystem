286722f7e83782a32edd7fdc74dc3643c5adcb40
al1922 <48806977+al1922@users.noreply.github.com>
2021-06-08 13:38:09 +0200

    Update Temp.py

--- Temp.py
+++ Temp.py
@@ -1,29 +1,16 @@
-from gmpy2 import powmod, mul, div
-from secrets import randbelow
-
-def encrypt(data, public_key):
-
-    """ This function encrypts your data """
-
-    g, n = public_key # Get public KEY
-    n2 = mul(n,n)  # n**2 used in next calculations.
-
-    r = randbelow(n-1) + 1 # Select random INTEGER g from Z. Where Z is set of values ​​from 1 to n**2.
+if __name__ == "__main__":
     
-    if type(data) == int : data = [data] # Change type to list if not.
-    
-    const_num = powmod(r, n, n2) # Const used to next calculation.
-    encrypted_numbers = [ mul(powmod(g, value, n2), const_num) % n2 for value in data ] # Compute ciphertext E(m) = g**m * r**n mod n**2 where m is number
+    private_key, public_key = generateKey(nbits = 1028) # Generate private and public key n-bits long.
 
-    return encrypted_numbers
+    data_1 = 29 # Example of data. 
+    data_2 = 31  # Example of data. 
 
-def decrypt(encrypted_data, private_key):
+    encrypt_data_1 = encrypt(data_1, public_key) # Encrypt data.
+    encrypt_data_2 = encrypt(data_2, public_key) # Encrypt data.
 
-    """ This function decrypts your data """
+    sum_encrypted_data = addEncrypted(encrypt_data_1, encrypt_data_2, public_key) # Add elements .
 
-    def eucDiv(val,n): # The quotient of the Euclidean division. 
-        return div(val - 1, n) 
+    score  = decrypt(sum_encrypted_data, private_key) # Decrypt data.
 
-    alpha, mu, n = private_key # Get private KEY
-
-    return mul(eucDiv(powmod(encrypted_data , alpha, mul(n, n)), n), mu) % n # m = eucDiv(m**alpha mod n**2, n) * mu mod n where m is your decrypted number.
+    print(score) 
+    >>> 60
