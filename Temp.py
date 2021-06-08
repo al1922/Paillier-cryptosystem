e427f3d9c16a61cdadb70752ea356976bf32bc8a
al1922 <48806977+al1922@users.noreply.github.com>
2021-06-08 03:28:35 +0200

    Create Temp.py

---
+++ Temp.py
@@ -0,0 +1,6 @@
+first_prime_number, second_prime_number = generatePrimeNumbers(128)
+print('First:', first_prime_number)
+print('Second:', second_prime_number)
+
+>>> First: 337230051474594181368563910213537840241
+>>> Second: 124251883157836474173103435494684186541
