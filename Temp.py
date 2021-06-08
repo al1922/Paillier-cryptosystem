ab224e9b441e0ac949c098ec4486a1cae322971c
al1922 <48806977+al1922@users.noreply.github.com>
2021-06-08 13:21:58 +0200

    Update Temp.py

--- Temp.py
+++ Temp.py
@@ -1,38 +1,29 @@
-from gmpy2 import gcd, lcm, invert, powmod, mul, div
+from gmpy2 import powmod, mul, div
 from secrets import randbelow
-from prime import generatePrimeNumbers 
 
+def encrypt(data, public_key):
 
+    """ This function encrypts your data """
 
-def generateKey(nbits):
+    g, n = public_key # Get public KEY
+    n2 = mul(n,n)  # n**2 used in next calculations.
+
+    r = randbelow(n-1) + 1 # Select random INTEGER g from Z. Where Z is set of values ​​from 1 to n**2.
+    
+    if type(data) == int : data = [data] # Change type to list if not.
+    
+    const_num = powmod(r, n, n2) # Const used to next calculation.
+    encrypted_numbers = [ mul(powmod(g, value, n2), const_num) % n2 for value in data ] # Compute ciphertext E(m) = g**m * r**n mod n**2 where m is number
+
+    return encrypted_numbers
+
+def decrypt(encrypted_data, private_key):
+
+    """ This function decrypts your data """
 
     def eucDiv(val,n): # The quotient of the Euclidean division. 
         return div(val - 1, n) 
 
-    while True:
+    alpha, mu, n = private_key # Get private KEY
 
-        """ Key generation scheme """
-
-        fisrt_prime, second_prime = generatePrimeNumbers(nbits) # Generate two prime Numbers of equivalent bit length p and q. 
-
-        while gcd(mul(fisrt_prime, second_prime), mul((fisrt_prime - 1), (second_prime - 1))) != 1:  # Confirm that gcd(p*q, (p − 1)*(q − 1)) = 1.
-            fisrt_prime, second_prime = generatePrimeNumbers(nbits) # If not, generate NEW prime numbers.
-
-        n = mul(fisrt_prime, second_prime) # Calculate n = p*q.
-        n2 = mul(n,n) # n**2 used in next calculations.
-
-        alpha = lcm(fisrt_prime - 1, second_prime - 1) # Calculate alpha =  lcm(p - 1, q - 1).
-
-        g = randbelow(n2-1) + 1  # Select random INTEGER g from Z. Where Z is set of values ​​from 1 to n**2.
-
-        try:
-
-            mu = invert(eucDiv(powmod(g, alpha, n2), n), n) # Calculate the modular multiplicative inverse. mu = (eucDiv( g**alpha mod n**2 ))**-1 mod n
-            
-            private_key = [alpha, mu, n] # Private KEY.
-            public_key = [g, n]          # Public KEY.
-            
-            return private_key, public_key
-
-        except Exception:
-            pass
+    return mul(eucDiv(powmod(encrypted_data , alpha, mul(n, n)), n), mu) % n # m = eucDiv(m**alpha mod n**2, n) * mu mod n where m is your decrypted number.
